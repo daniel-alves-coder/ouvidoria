ae1f48ae7d5c7e006a97297f74aaaeb473446782
Daniel <daniel.alves.costa2006@gmail.com>
2025-04-09 20:00:17 -0300

    adicionei as instrunçoes de como criar a tabela no mysql

---
+++ bd.py
@@ -0,0 +1,20 @@
+'''
+cria um banco de dados com o nome: ouvidoria
+'''
+
+'''
+codigo para criar uma tabela: 
+------------------------------
+
+create table manifestacoes (
+    codigo int auto_incremente,
+    nome_usuario varchar(100),
+    manifestacao varchar(500),
+    tipo varchar(20),
+    primay key(codigo)
+);
+'''
+
+'''
+troque a senha na linha 6 para a senha do seu mysql
+'''
