e79a16dbe5e0b529554ae4cdaa646d848f6fa76d
Daniel <daniel.alves.costa2006@gmail.com>
2025-04-09 19:20:54 -0300

    alterando a arquivo final.py

--- final.py
+++ final.py
@@ -29,26 +29,37 @@ while opcao != 7:
         print(barra * 2)
 
     elif opcao == 2: # Listagem das Manifestações por tipo
-        print("ESCOLHA UM TIPO DE MANIFESTAÇÃO \n1) Elogio \n2) Sugestão \n3) Reclamação")
-        print(barra)
-        tipo = int(input("Digite o codigo: "))
         sql = "select * from manifestacoes where tipo = %s"
 
-        if tipo == 1:
-            dados = ["Elogio"]
-        elif tipo == 2:
-            dados = ["Sugestão"]
-        elif tipo == 3:
-            dados = ["Reclamação"]
-        else:
-            print("TIPO INVALIDO")
+        while True:
+            print("ESCOLHA UM TIPO DE MANIFESTAÇÃO \n1) Elogio \n2) Sugestão \n3) Reclamação")
+            print(barra)
+            tipo = int(input("Digite o codigo: "))
+            print(barra)
+
+            if tipo == 1:
+                tipo = "Elogio"
+                break
+            elif tipo == 2:
+                tipo = "Sugestão"
+                break
+            elif tipo == 3:
+                tipo = "Reclamação"
+                break
+            else:
+                print("CÓDIGO INVALIDO")
+                print(barra)
+        dados = [tipo]
 
         retorno = listarBancoDados(conexao,sql,dados)
 
-        print(barra * 2)
-        for item in retorno:
-            print("CODIGO:", item[0], "|", "NOME:", item[1], "|", "MANIFESTAÇÃO:", item[2], "|", "TIPO:", item[3], "|")
-        print(barra * 2)
+        if len(retorno) == 0:
+            print("Não existe nunhum", tipo)
+        else:
+            print(barra * 2)
+            for item in retorno:
+                print("CODIGO:", item[0], "|", "NOME:", item[1], "|", "MANIFESTAÇÃO:", item[2], "|", "TIPO:", item[3], "|")
+            print(barra * 2)
 
     elif opcao == 3: # Criar uma nova manifestação
         sql = "insert into manifestacoes (nome_usuario,manifestacao,tipo) values (%s,%s,%s);"
@@ -56,19 +67,24 @@ while opcao != 7:
         nomeUsuario = input("Digite o nome do usuario que inseriu a manifestação:")
         print(barra)
 
-        print("ESCOLHA UM TIPO DE MANIFESTAÇÃO \n1) Elogio \n2) Sugestão \n3) Reclamação")
-        print(barra)
-        tipo = int(input("Digite o codigo: "))
-        print(barra)
+        while True:
+            print("ESCOLHA UM TIPO DE MANIFESTAÇÃO \n1) Elogio \n2) Sugestão \n3) Reclamação")
+            print(barra)
+            tipo = int(input("Digite o codigo: "))
+            print(barra)
 
-        if tipo == 1:
-            tipo = "elogio"
-        elif tipo == 2:
-            tipo = "Sugestão"
-        elif tipo == 3:
-            tipo = "Reclamação"
-        else:
-            print("TIPO INVALIDO")
+            if tipo == 1:
+                tipo = "Elogio"
+                break
+            elif tipo == 2:
+                tipo = "Sugestão"
+                break
+            elif tipo == 3:
+                tipo = "Reclamação"
+                break
+            else:
+                print("CÓDIGO INVALIDO")
+                print(barra)
 
         manifestacao = input("Digite sua manifestação: ")
 
@@ -90,19 +106,21 @@ while opcao != 7:
                 print("Até o momento, o sistema possui exatas " + str(item[0]) + " manifestações")
 
     elif opcao == 5: # Pesquisar uma manifestação por código
-        sql = "select * from manifestacoes where código = %s"
+        sql = "select * from manifestacoes where codigo = %s"
         codigo = int(input("Digite o codigo da manifestação: "))
         print(barra)
         dados = [codigo]
-
         retorno = listarBancoDados(conexao, sql, dados)
 
-        print(barra*2)
-        for item in retorno:
-            print("CÓDIGO:",item[0],"|","NOME:",item[1],"|","MANIFESTAÇÃO:",item[2],"|","TIPO:",item[3],"|")
-        print(barra*2)
+        if len(retorno) == 0:
+            print("Não existe nunhuma manifestação com esse código")
+        else:
+            print(barra*2)
+            for item in retorno:
+                print("CÓDIGO:",item[0],"|","NOME:",item[1],"|","MANIFESTAÇÃO:",item[2],"|","TIPO:",item[3],"|")
+            print(barra*2)
 
-    elif opcao == 6:
+    elif opcao == 6: # Delete manifestação por codigo
         sql = "delete from manifestacoes where codigo = %s"
         codigo = int(input("Digite o código da Manifestação: "))
         dados = [codigo]
